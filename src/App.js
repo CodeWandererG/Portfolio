6fa5d8ec5660ff2e713bae1c49fce9fa6b0fd6c1
GurjeetSingh7000 <101127973+GurjeetSingh7000@users.noreply.github.com>
2024-05-23 22:50:55 +0530

    main component changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import About from './components/About';
+import Interests from './components/Interests';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Sidebar />
+      <div className="main-content">
+        <Navbar/>
+        <About />
+        <Interests />
+      </div>
     </div>
   );
 }
